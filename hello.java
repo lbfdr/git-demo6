56f212fb0ef3f36d89a6cb5cbe7613fe6f00e180
lbfdr <123924276+lbfdr@users.noreply.github.com>
2023-03-27 21:55:30 +0800

    Create hello.java

--- hello.java
+++ hello.java
@@ -1,9 +1,10 @@
 public class HelloTest{
-
 	@test
 	public void test(){}
 	}
-
+        @test
+	public void test1(){}
+	}
 	public static void main(string[] args){
 		System.out.print("helloWorld");
 		}
